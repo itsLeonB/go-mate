48ccb0d46ebda1bd50abbb7d311a0819a8da8e62
itsLeonB <ellionblessan@gmail.com>
2024-12-10 23:21:55 +0700

    feat: add indexes

--- db/migrations/down.sql
+++ db/migrations/down.sql
@@ -1,3 +1,7 @@
+DROP INDEX IF EXISTS recommendation_logs_user_recommended_created_idx;
+
+DROP INDEX IF EXISTS recommendation_logs_user_id_created_at_idx;
+
 DROP TABLE IF EXISTS recommendation_logs;
 
 DROP TYPE recommendation_log_status;
--- db/migrations/up.sql
+++ db/migrations/up.sql
@@ -13,10 +13,16 @@ CREATE TYPE recommendation_log_status AS ENUM ('viewed', 'liked', 'passed');
 
 CREATE TABLE IF NOT EXISTS recommendation_logs (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL,
-    recommended_user_id UUID NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id),
+    recommended_user_id UUID NOT NULL REFERENCES users(id),
     status recommendation_log_status NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     deleted_at TIMESTAMPTZ
 );
+
+CREATE INDEX recommendation_logs_user_id_created_at_idx
+ON recommendation_logs (user_id, created_at);
+
+CREATE INDEX recommendation_logs_user_recommended_created_idx
+ON recommendation_logs (user_id, recommended_user_id, created_at);
